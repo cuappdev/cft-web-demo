98526332f491618235626ef042aec638d3707e8b
Peter <mrpeterssyt@gmail.com>
2024-11-08 22:50:38 -0500

    initial commit

---
+++ types/Member.ts
@@ -0,0 +1,77 @@
+export type Member = {
+  name: string;
+  image: string;
+  subteam: string;
+  position: string;
+}
+
+const members: Member[] = [
+  {
+    name: "Aayush Agnihotri",
+    image: "aayush.jpeg",
+    subteam: "Backend",
+    position: "Backend Lead",
+  },
+  {
+    name: "Ant Torres",
+    image: "ant.png",
+    subteam: "iOS",
+    position: "Product Lead",
+  },
+  {
+    name: "Caitlyn Jin",
+    image: "caitlyn.png",
+    subteam: "iOS",
+    position: "Member",
+  },
+  {
+    name: "Candy Wu",
+    image: "candy.jpeg",
+    subteam: "Marketing",
+    position: "Marketing Lead",
+  },
+  {
+    name: "Charles Liggins",
+    image: "charles.jpg",
+    subteam: "iOS",
+    position: "Member",
+  },
+  {
+    name: "Jennifer Gu",
+    image: "jennifer.jpg",
+    subteam: "iOS",
+    position: "Vice Team Lead",
+  },
+  {
+    name: "Justin Guo",
+    image: "justin.jpg",
+    subteam: "Android",
+    position: "Android Lead",
+  },
+  {
+    name: "Matt Wong",
+    image: "matt.png",
+    subteam: "Marketing",
+    position: "Team Lead",
+  },
+  {
+    name: "Nancy Duong",
+    image: "nancy.png",
+    subteam: "Design",
+    position: "Design Lead",
+  },
+  {
+    name: "Peter Bidoshi",
+    image: "peter.png",
+    subteam: "iOS",
+    position: "Course Instructor",
+  },
+  {
+    name: "Richie Sun",
+    image: "richie.jpg",
+    subteam: "iOS",
+    position: "iOS Lead",
+  },
+];
+
+export default members;
