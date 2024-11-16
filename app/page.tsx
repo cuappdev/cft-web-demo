dbdf9665802ba0c61b6cc49ae7ccc6a35a79a583
Peter <mrpeterssyt@gmail.com>
2024-11-16 13:03:34 -0500

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import MemberCard from "@/components/MemberCard";
-import { Member, members } from "@/types/Member";
 
 export default function Home() {
 
+  let endpoint = "https://chatdev-wuzwgwv35a-ue.a.run.app/api/"
+
   return (
     <div className="flex flex-col justify-center p-16 gap-4">
-      <h1 className="text-2xl text-center">Members</h1>
+      <h1 className="text-2xl text-center">Posts</h1>
       <div className="grid grid-cols-3 gap-4">
-        {members.map((member: Member) => (
-          <MemberCard member={member} key={member.name} />
-        ))}
       </div>
     </div>
   );
