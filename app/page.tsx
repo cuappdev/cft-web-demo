f8a46e4f0a3a6a84e05cd44ba6c86223233b9ca2
Peter <mrpeterssyt@gmail.com>
2024-11-16 13:46:29 -0500

    final

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
+"use client";
+
+import CreatePostCell from "@/components/CreatePostCell";
+import PostCell from "@/components/PostCell";
+import { parsePosts } from "@/helper/Parse";
+import { Post } from "@/types/Posts";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  let endpoint = "https://chatdev-wuzwgwv35a-ue.a.run.app/api";
+  
+  const [posts, setPosts] = useState<Post[]>([]);
 
-  let endpoint = "https://chatdev-wuzwgwv35a-ue.a.run.app/api/"
+  useEffect(() => {
+    fetch(`${endpoint}/posts`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(parsePosts(data));
+      });
+  }, []);
 
   return (
     <div className="flex flex-col justify-center p-16 gap-4">
       <h1 className="text-2xl text-center">Posts</h1>
       <div className="grid grid-cols-3 gap-4">
+        <CreatePostCell />
+        {posts.map((post) => (
+          <PostCell key={post.id} post={post} />
+        ))}
       </div>
     </div>
   );
